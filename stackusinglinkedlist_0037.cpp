eefd805f3783ad4baf7816622f6e3870eddd5ece
prdpta <pradiptaputramei01@gmail.com>
2024-05-11 10:20:08 +0700

    menambahkan if top == NULL

--- stackusinglinkedlist_0037.cpp
+++ stackusinglinkedlist_0037.cpp
@@ -45,3 +45,6 @@ public:
 
     // Peek/Top operation: Retrieve the value of the topmost element without removing it
     void peek() {
+        if (top == NULL) {
+            cout << "List is empty." << endl;
+        }
