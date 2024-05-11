b241a58f0a8ca109b96da50905cab141d4e5e90e
prdpta <pradiptaputramei01@gmail.com>
2024-05-11 10:22:38 +0700

    menambahkan while

--- stackusinglinkedlist_0037.cpp
+++ stackusinglinkedlist_0037.cpp
@@ -50,3 +50,10 @@ public:
         }
         else {
             Node* current = top;
+            while (current != NULL) {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        } // Return the value of the top node
+    }
