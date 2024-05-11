846c01b65f47f80bc019e28e1edb7742a80ee2f2
prdpta <pradiptaputramei01@gmail.com>
2024-05-11 10:29:56 +0700

    menambahkan default

--- stackusinglinkedlist_0037.cpp
+++ stackusinglinkedlist_0037.cpp
@@ -105,3 +105,7 @@ int main() {
             case 4:
                 cout << "Exiting program." << endl;
                 break;
+            default:
+                cout << "Invalid choice. Try again." << endl;
+                break;
+        }
