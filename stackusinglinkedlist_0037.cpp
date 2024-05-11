d3c9feca77c501371f3277851684723974bfda55
prdpta <pradiptaputramei01@gmail.com>
2024-05-11 10:16:10 +0700

    menambahkan int push

--- stackusinglinkedlist_0037.cpp
+++ stackusinglinkedlist_0037.cpp
@@ -22,3 +22,13 @@ public:
     Stack() {
         top = NULL; // Initialize the stack with a null top pointer
     }
+
+    // Push operation: Insert an element onto the top of the stack
+    int push(int value) {
+        Node* newNode = new Node(); // 1. Allocate memory for the new node
+        newNode->data = value; // 2. Assign value
+        newNode->next = top; // 3. Set the next pointer of the new node to the current top node
+        top = newNode; // 4. Update the top pointer to the new node
+        cout << "Push value: " << value << endl;
+        return value;
+    }
